ff5b49f57b5bfa015ff17960e44c3c209090ace8
inved1 <inved1@bfh.ch>
2013-09-25 16:21:05 +0200

    tested some client-server stuff

---
+++ myClassLibServer/ServerClassValue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myClassLibServer
+{
+    public class ServerClassValue
+    {
+    }
+
+    public class HelloServer : MarshalByRefObject
+    {
+        public void HelloMethod(ForwardMe o)
+        {
+            o.CallMe();
+        }
+
+
+    }
+
+    [Serializable]
+    public class ForwardMe
+    {
+
+        public  void CallMe()
+        {   
+ 	        Console.WriteLine("CallMe was executed in: " + Process.GetCurrentProcess().ProcessName.ToString());
+
+        }
+    }
+}
