43894707ea925533c15ccf21d58821d0e32ee668
inved1 <inved1@bfh.ch>
2013-10-18 09:03:15 +0200

    add tester

---
+++ monopoly.prototype.tester/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace monopoly.prototype.tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            monopoly.prototype.client.FrmClient f = new client.FrmClient();
+            f.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            monopoly.prototype.server.FrmServer f = new server.FrmServer();
+            f.Show();
+
+        }
+    }
+}
