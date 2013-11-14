09d621c39b0baa49818bfccc010514a91daebd99
fankm9@bfh.ch <fankm9@bfh.ch>
2013-11-14 18:58:52 +0100

    actions added

---
+++ monopoly.prototypeV2.logic/classes/actions/cActionGiveUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using monopoly.prototypeV2.logic.interfaces;
+
+namespace monopoly.prototypeV2.logic.classes
+{
+    [Serializable] 
+    public class cActionGiveUp:IAction
+    {
+        const string ACTION_NAME = "Zug beenden";
+        private cGame game = null;
+
+        public cActionGiveUp(cGame game)
+        {
+            this.game = game;
+        }
+
+        public string getName()
+        {
+            return ACTION_NAME;
+        }
+
+        public void runAction()
+        {
+
+        }
+    }
+}
---
+++ monopoly.prototypeV2.logic/classes/actions/cActionLogin.cs
@@ -0,0 +1,31 @@
+﻿using monopoly.prototypeV2.logic.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monopoly.prototypeV2.logic.classes
+{
+    [Serializable]
+    public class cActionLogin : IAction
+    {
+        const string ACTION_NAME = "Login";
+        private cGame game = null;
+
+        public cActionLogin(cGame game)
+        {
+            this.game = game;
+        }
+
+        public string getName()
+        {
+            return ACTION_NAME;
+        }
+
+        public void runAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
