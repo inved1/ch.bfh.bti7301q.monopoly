329add17d3c59f1c4cb935fa84fef046d686ab7e
inved1 <inved1@bfh.ch>
2013-12-13 15:22:24 +0100

    11123123

--- monopoly.prototypeV2.client/form/frmClientGame_V02.cs
+++ monopoly.prototypeV2.client/form/frmClientGame_V02.cs
@@ -269,15 +269,15 @@ namespace monopoly.prototypeV2.client.form
 
         public void onUpdateGUIActionsEvent(object sender, EventArgs e)
         {
-            //MessageBox.Show("asd");
-            //if (this.InvokeRequired) {
-            //    cbGUIAction d = new cbGUIAction(onUpdateGUIActionsEvent);
-            //    this.Invoke(d,new object[] {sender,e});
+            //  MessageBox.Show("asd");
+            if (this.InvokeRequired) {
+                cbGUIAction d = new cbGUIAction(onUpdateGUIActionsEvent);
+                this.Invoke(d,new object[] {sender,e});
 
 
-            //}
-            //else
-            //{
+            }
+            else
+            {
 
                 frmGenericActions f = new frmGenericActions();
                 foreach (IAction action in this.myGame.Actions)
@@ -298,7 +298,7 @@ namespace monopoly.prototypeV2.client.form
                     f.Close();
                 }
                 f.Dispose();
-            //}
+            }
         }
         public void onUpdateGUIEvent(object sender, EventArgs e)
         {
